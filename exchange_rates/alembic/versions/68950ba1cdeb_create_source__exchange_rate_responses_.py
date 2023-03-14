a7af7c8ed475773b0ab906a99534966d8d24c5b5
duncangarde <duncan.garde@korukids.co.uk>
2023-03-14 21:26:54 +0000

    Migrate responses table
    Migrates a responses table into the DB with the columns used for storage

---
+++ exchange_rates/alembic/versions/68950ba1cdeb_create_source__exchange_rate_responses_.py
@@ -0,0 +1,31 @@
+"""Create source__exchange_rate_responses table
+
+Revision ID: 68950ba1cdeb
+Revises: 
+Create Date: 2023-03-11 22:02:03.703247
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import JSONB
+
+
+# revision identifiers, used by Alembic.
+revision = '68950ba1cdeb'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'source__exchange_rate_responses',
+        sa.Column('timestamp', sa.BigInteger, primary_key=True),
+        sa.Column('base', sa.String(50), nullable=False),
+        sa.Column('date', sa.Date, nullable=False),
+        sa.Column('rates', sa.JSON, nullable=False),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table('source__exchange_rate_responses')
